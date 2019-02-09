b100cf1c9d2f329c3fbe444b719cb3139b643118
Marine <marine.moynet@epitech.eu>
2019-02-09 15:01:00 +0100

    JS Marine

--- wysiwyg.js
+++ wysiwyg.js
@@ -18,19 +18,25 @@ function createButtons(){
 }
 
 function getSelectedContent(){
-    
-    $('#underline').click(function(){
-        let textarea = $("#textarea");
-        let start = textarea[0].selectionStart;
-        console.log(start);
-        let finish = textarea[0].selectionEnd;
-        console.log(finish);
-        let sel = textarea.val();
-        let lol = sel.substring(start, finish);
-        console.log(lol);
+    document.designMode ='on';
+    $('#textarea').focus();
+    $('#underline').click(function(e){
+        e.preventDefault();
+        document.execCommand('underline', false, null);
+        $('#textarea').focus();
+        return false;
+        // let textarea = $("#textarea");
+        // let start = textarea[0].selectionStart;
+        // console.log(start);
+        // let finish = textarea[0].selectionEnd;
+        // console.log(finish);
+        // let sel = textarea.val();
+        // let lol = sel.substring(start, finish);
+        // console.log(lol);
         // let underline = $(this).css('text-decoration', 'underline');
         // $(lol).html(underline);
-        $(lol).wrap('<span style="text-decoration: underline;">');
+        //document.$(lol).execCommand('underline');
+        //$(lol).wrapInner('<span />');
         //$(lol).html('<span style="text-decoration: underline;">')
         //$(lol).addClass('underline');
     }); 
